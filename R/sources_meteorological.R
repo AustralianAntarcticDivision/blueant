2e6f470d7b69af49f41ab711291b19c2022297a8
Ben Raymond <ben.raymond@aad.gov.au>
2017-10-05 14:53:09 +1100

    remove amps source_url, is now hard coded in handler

--- R/sources_meteorological.R
+++ R/sources_meteorological.R
@@ -5,7 +5,6 @@ sources_meteorological <- function() {
         description="The Antarctic Mesoscale Prediction System - AMPS - is an experimental, real-time numerical weather prediction capability that provides support for the United States Antarctic Program, Antarctic science, and international Antarctic efforts.",
         reference="http://www2.mmm.ucar.edu/rt/amps/",
         citation="See http://www2.mmm.ucar.edu/rt/amps/",
-        source_url="http://www2.mmm.ucar.edu/rt/amps/wrf_grib/",
         license="Please cite",
         comment="d1,d2 files for hours 000-027 only. Note that this web site provides only the last few days of files.",
         method=quote(amps_get),
