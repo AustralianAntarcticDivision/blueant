74fdaf890a853186087a3c245cc0d42d96e29440
Ben Raymond <ben.raymond@aad.gov.au>
2018-02-06 10:22:41 +1100

    update imports

--- R/blueant.R
+++ R/blueant.R
@@ -4,11 +4,13 @@
 #' @docType package
 #' @references \url{http://data.aad.gov.au}
 #' @importFrom assertthat assert_that is.flag is.string
-#' @importFrom bowerbird bb_data_source_dir bb_data_sources bb_source bb_handler_wget bb_install_wget
+#' @importFrom bowerbird bb_data_source_dir bb_data_sources bb_data_sources<- bb_source bb_handler_wget bb_handler_oceandata bb_install_wget bb_settings
 #' @importFrom magrittr %>%
+#' @importFrom methods is
 #' @importFrom rvest html_session jump_to html_attr html_nodes
 #' @importFrom stats na.omit
 #' @importFrom stringr str_match str_trim
+#' @importFrom utils unzip
 #' @importFrom xml2 url_absolute
 NULL
 
