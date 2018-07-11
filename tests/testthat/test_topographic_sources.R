64128641b6ecd778f815b6b8146215204015f5ff
Ben Raymond <ben.raymond@aad.gov.au>
2018-07-11 19:34:06 +1000

    tweak topo source test

--- tests/testthat/test_topographic_sources.R
+++ tests/testthat/test_topographic_sources.R
@@ -7,10 +7,19 @@ test_that("source nsidc0082 still works under ftp (due to be moved to https)",{
     tmp <- sources(name="Radarsat Antarctic digital elevation model V2")
     tmp$source_url[[1]] <- "ftp://sidads.colorado.edu/pub/DATASETS/nsidc0082_radarsat_dem_v02/200M/BINARY/ramp200dem_osu_v2.hdr"
     cf <- bb_add(cf,tmp)
-    bb_sync(cf,confirm_downloads_larger_than=-1)
-    fnm <- file.path(temp_root,"sidads.colorado.edu/pub/DATASETS/nsidc0082_radarsat_dem_v02/200M/BINARY/ramp200dem_osu_v2.hdr")
-    expect_true(file.exists(fnm))
-    fi <- file.info(fnm)
-    expect_gt(fi$size,1e3)
+    status <- list(status = FALSE)
+    tries <- 0
+    while (!status$status && tries < 3) {
+        tries <- tries + 1
+        status <- bb_sync(cf,confirm_downloads_larger_than=-1)
+    }
+    if (!status$status) {
+        stop("nsidc0082 test failed to download file")
+    } else {
+        fnm <- file.path(temp_root,"sidads.colorado.edu/pub/DATASETS/nsidc0082_radarsat_dem_v02/200M/BINARY/ramp200dem_osu_v2.hdr")
+        expect_true(file.exists(fnm))
+        fi <- file.info(fnm)
+        expect_gt(fi$size,1e3)
+    }
 })
 
