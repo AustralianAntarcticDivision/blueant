3837731ac0062baeffec812b865d6558fa10cc5b
Ben Raymond <ben.raymond@aad.gov.au>
2018-08-14 15:37:13 +1000

    remove windows wget install on testing

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,11 +1,4 @@
 library(testthat)
 library(blueant)
 
-if (.Platform$OS.type=="windows") {
-    if (is.null(bb_find_wget(error=FALSE))) {
-        warning("At init of testing: could not find wget executable, installing.\n")
-        bb_install_wget()
-        tmp <- bb_find_wget() ## will throw error if wget not found
-    }
-}
 test_check("blueant")
