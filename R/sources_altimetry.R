9c4ed84e63ef490c5a16f06467a75cbec8ad7ce6
Ben Raymond <ben.raymond@aad.gov.au>
2019-02-12 16:25:19 +1100

    add AVISO Finite-Size Lyapunov Exponent data

--- R/sources_altimetry.R
+++ R/sources_altimetry.R
@@ -6,6 +6,8 @@
 #'   \item "CMEMS global gridded SSH reprocessed (1993-ongoing)": Global Ocean - Multimission altimeter satellite gridded sea surface heights and derived variables computed with respect to a twenty-year mean. All the missions are homogenized with respect to a reference mission which is currently OSTM/Jason-2
 #'   \item "CMEMS global gridded SSH near-real-time": near-real-time version of 'CMEMS global gridded SSH reprocessed (1993-ongoing)'
 #'   \item "CNES-CLS2013 Mean Dynamic Topography": CNES-CLS2013 Mean dynamic topography over the 1993-2012 period of the sea surface height above geoid. The MDT_CNES-CLS13 is an estimate of the ocean MDT for the 1993-2012 period. Since April 2014 (Duacs 2014, v15.0 version), the Ssalto/Duacs (M)SLA products are computed relative to 1993-2012 period that is consistent with this new MDT CNES-CLS13. Based on 2 years of GOCE data, 7 years of GRACE data, and 20 years of altimetry and in-situ data (hydrologic and drifters data)
+#'   \item "Near-real-time finite size Lyapunov exponents": These products provide the exponential rate of separation of particle trajectories initialized nearby and advected by altimetry velocities. FSLEs highlight the transport barriers that control the horizontal exchange of water in and out of eddy cores.
+#'   \item "Delayed-time finite size Lyapunov exponents": These products provide the exponential rate of separation of particle trajectories initialized nearby and advected by altimetry velocities. FSLEs highlight the transport barriers that control the horizontal exchange of water in and out of eddy cores.
 #' }
 #'
 #' The returned tibble contains more information about each source.
@@ -91,7 +93,7 @@ sources_altimetry <- function(name,formats,time_resolutions, ...) {
                          doc_url = "https://www.aviso.altimetry.fr/en/data/products/auxiliary-products/mdt.html",
                          citation = "Rio, M-H, P. Schaeffer, G. Moreaux, J-M Lemoine, E. Bronner (2009) : A new Mean Dynamic Topography computed over the global ocean from GRACE data, altimetry and in-situ measurements . Poster communication at OceanObs09 symposium, 21-25 September 2009, Venice.",
                          source_url = "ftp://ftp-access.aviso.altimetry.fr/auxiliary/mdt/mdt_cnes_cls2013_global/",
-                         license = "See https://www.aviso.altimetry.fr/en/data/data-access/endatadata-accessregistration-form.html",
+                         license = "See https://www.aviso.altimetry.fr/fileadmin/documents/data/License_Aviso.pdf",
                          ##method = list("bb_handler_wget"), ## --recursive --level=1 --no-parent
                          method = list("bb_handler_rget", level = 1),
                          postprocess = list("bb_gunzip"),
@@ -100,6 +102,48 @@ sources_altimetry <- function(name,formats,time_resolutions, ...) {
                          password = "",
                          access_function = "raster",
                          collection_size = 0.1,
+                         data_group = "Altimetry", warn_empty_auth = FALSE))
+    }
+
+    if (is.null(name) || any(name %in% tolower(c("Near-real-time finite size Lyapunov exponents", "FSLE NRT")))) {
+        ## https://www.aviso.altimetry.fr/fileadmin/documents/data/tools/FSLE_handbook.pdf
+        out <- rbind(out,
+                     bb_source(
+                         name = "Near-real-time finite size Lyapunov exponents",
+                         id = "FLSE NRT",
+                         description = "The maps of Backward-in-time, Finite-Size Lyapunov Exponents (FSLEs) and Orientations of associated eigenvectors are computed over 21-year altimetry period and over global ocean within the SALP/Cnes project in collaboration with CLS, LOcean and CTOH. These products provide the exponential rate of separation of particle trajectories initialized nearby and advected by altimetry velocities. FSLEs highlight the transport barriers that control the horizontal exchange of water in and out of eddy cores.",
+                         doc_url = "https://www.aviso.altimetry.fr/en/data/products/value-added-products/fsle-finite-size-lyapunov-exponents/fsle-description.html",
+                         citation = "D'Ovidio F, Lopez C, Hernandez-Garcia E, Fernandez V (2004) Mixing structures in the Mediterranean sea from Finite-Size Lyapunov Exponents. Geophys. Res. Lett., 31, L17203",
+                         source_url = "ftp://ftp-access.aviso.altimetry.fr/value-added/lyapunov/near-real-time/global/",
+                         license = "See https://www.aviso.altimetry.fr/fileadmin/documents/data/License_Aviso.pdf",
+                         method = list("bb_handler_rget", level = 2),
+                         authentication_note = "AVISO login required, see https://www.aviso.altimetry.fr/en/data/data-access/endatadata-accessregistration-form.html",
+                         user = "",
+                         password = "",
+                         access_function = "raster",
+                         collection_size = 100,
+                         comment = "File size 140MB per file, approx 50 GB per year",
+                         data_group = "Altimetry", warn_empty_auth = FALSE))
+    }
+    if (is.null(name) || any(name %in% tolower(c("Delayed-time finite size Lyapunov exponents", "FSLE DT")))) {
+        ## https://www.aviso.altimetry.fr/fileadmin/documents/data/tools/FSLE_handbook.pdf
+        out <- rbind(out,
+                     bb_source(
+                         name = "Delayed-time finite size Lyapunov exponents",
+                         id = "FLSE DT",
+                         description = "The maps of Backward-in-time, Finite-Size Lyapunov Exponents (FSLEs) and Orientations of associated eigenvectors are computed over 21-year altimetry period and over global ocean within the SALP/Cnes project in collaboration with CLS, LOcean and CTOH. These products provide the exponential rate of separation of particle trajectories initialized nearby and advected by altimetry velocities. FSLEs highlight the transport barriers that control the horizontal exchange of water in and out of eddy cores.",
+                         doc_url = "https://www.aviso.altimetry.fr/en/data/products/value-added-products/fsle-finite-size-lyapunov-exponents/fsle-description.html",
+                         citation = "D'Ovidio F, Lopez C, Hernandez-Garcia E, Fernandez V (2004) Mixing structures in the Mediterranean sea from Finite-Size Lyapunov Exponents. Geophys. Res. Lett., 31, L17203",
+                         source_url = "ftp://ftp-access.aviso.altimetry.fr/value-added/lyapunov/delayed-time/global/",
+                         license = "See https://www.aviso.altimetry.fr/fileadmin/documents/data/License_Aviso.pdf",
+                         method = list("bb_handler_rget", level = 2),
+                         ##method = list("bb_handler_wget", level = 2),
+                         authentication_note = "AVISO login required, see https://www.aviso.altimetry.fr/en/data/data-access/endatadata-accessregistration-form.html",
+                         user = "",
+                         password = "",
+                         access_function = "raster",
+                         comment = "File size 140MB per file, approx 50 GB per year",
+                         collection_size = 1200,
                          data_group = "Altimetry", warn_empty_auth = FALSE))
     }
     out
