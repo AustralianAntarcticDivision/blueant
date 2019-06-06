aa682cadf5187476d675c8b3d3b109dbe63f010d
Ben Raymond <ben.raymond@aad.gov.au>
2019-06-06 10:01:15 +1000

    GHRSST source changed from podaac-ftp.jpl.nasa.gov to ftp.nodc.noaa.gov

--- R/ghrsst.R
+++ R/ghrsst.R
@@ -1,8 +1,8 @@
 #' Handler for GHRSST data sources
 #'
-#' This is a handler function to be used with GHRSST data from podaac-ftp.jpl.nasa.gov. This function is not intended to be called directly, but rather is specified as a \code{method} option in \code{\link{bb_source}}.
+#' This is a handler function to be used with GHRSST data from ftp.nodc.noaa.gov. This function is not intended to be called directly, but rather is specified as a \code{method} option in \code{\link{bb_source}}.
 #'
-#' @references https://podaac.jpl.nasa.gov/Multi-scale_Ultra-high_Resolution_MUR-SST
+#' @references https://podaac.jpl.nasa.gov/dataset/MUR-JPL-L4-GLOB-v4.1
 #' @param ... : parameters passed to \code{\link{bb_wget}}
 #'
 #' @return TRUE on success
@@ -20,7 +20,7 @@ bb_handler_ghrsst <- function(...) {
 # @return TRUE on success or the directory name if local_dir_only is TRUE
 bb_handler_ghrsst_inner <- function(config,verbose=FALSE,local_dir_only=FALSE,...) {
 
-    ## The data source is ftp://podaac-ftp.jpl.nasa.gov/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/
+    ## The data source is ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/GDS2/L4/GLOB/JPL/MUR/v4.1/
     ## with yearly subdirectories
     ## within yearly directories, days are subdirectories
     ## BUT the daily subdirectories are symlinks and recursive wget won't recurse symlinked directories (known limitation of wget)
@@ -31,9 +31,9 @@ bb_handler_ghrsst_inner <- function(config,verbose=FALSE,local_dir_only=FALSE,..
     ## wget --recursive --level=inf --no-parent --timestamping ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/L4/GLOB/JPL/MUR/2015/051/
 
     ## we expect that the provided source URL is either pointing to the root of the collection:
-    ## ftp://podaac-ftp.jpl.nasa.gov/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/
+    ## ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/GDS2/L4/GLOB/JPL/MUR/v4.1/
     ## or to a particular year (multiple years will need multiple source_urls)
-    ## ftp://podaac-ftp.jpl.nasa.gov/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/2015/
+    ## ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/GDS2/L4/GLOB/JPL/MUR/v4.1/2015/
     ##
     ## for the former, we will loop from 2002 to present here
     ## for the latter, just hit that specific yearly directory
@@ -50,7 +50,7 @@ bb_handler_ghrsst_inner <- function(config,verbose=FALSE,local_dir_only=FALSE,..
             ## all years, so return the root dir
             dummy <- config
             temp <- bb_data_sources(dummy)
-            temp$source_url <- "ftp://podaac-ftp.jpl.nasa.gov/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/"
+            temp$source_url <- "ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/GDS2/L4/GLOB/JPL/MUR/v4.1/"
             bb_data_sources(dummy) <- temp
             return(bb_handler_wget(dummy,verbose=verbose,local_dir_only=TRUE,...))
         }
@@ -68,7 +68,7 @@ bb_handler_ghrsst_inner <- function(config,verbose=FALSE,local_dir_only=FALSE,..
         for (thisday in daylist) {
             dummy <- config
             temp <- bb_data_sources(dummy)
-            temp$source_url <- paste0("ftp://podaac-ftp.jpl.nasa.gov/allData/ghrsst/data/GDS2/L4/GLOB/JPL/MUR/v4.1/",thisyear,"/",sprintf("%03d",thisday),"/")
+            temp$source_url <- paste0("ftp://ftp.nodc.noaa.gov/pub/data.nodc/ghrsst/GDS2/L4/GLOB/JPL/MUR/v4.1/",thisyear,"/",sprintf("%03d",thisday),"/")
             bb_data_sources(dummy) <- temp
             ## make sure to be using --recursive --no-parent
             this_status <- bb_handler_wget(dummy,verbose=verbose,recursive=TRUE,no_parent=TRUE,...)
