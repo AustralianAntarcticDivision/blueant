2cdee16e737f140d380541840340e0658c693c88
Ben Raymond <ben@theraymonds.org>
2020-03-26 10:46:41 +1100

    new aadc handler functions

--- R/aadc.R
+++ R/aadc.R
@@ -1,7 +1,15 @@
-get_aadc_doi <- function(metadata_record_url) {
-    doi <- suppressWarnings(readLines(metadata_record_url))
-    doi <- unique(na.omit(str_match(doi, ">doi:(10[^<]+)<")[, 2]))
-    if (length(doi) == 1) doi else NULL
+get_aadc_md <- function(metadata_id) {
+    get_aadc_json(paste0("https://data.aad.gov.au/metadata/records/", metadata_id, "?format=json"))
+}
+
+get_aadc_doi <- function(metadata_id) {
+    md <- get_aadc_md(metadata_id)
+    tryCatch(sub("^doi:", "", md$data$data_set_citation$dataset_doi), error = function(e) NULL)
+}
+
+get_aadc_json <- function(url) {
+    out <- curl::curl_fetch_memory(url, handle = curl::new_handle(ssl_verifypeer = 0L))
+    jsonlite::fromJSON(rawToChar(out$content))
 }
 
 #' Handler for files downloaded from the Australian Antarctic Data Centre EDS system
@@ -16,6 +24,72 @@ get_aadc_doi <- function(metadata_record_url) {
 #'
 #' @export
 bb_handler_aadc <- function(...) {
-    stop("bb_handler_aadc is hard-deprecated due to changes on the AADC servers. Use bb_handler_rget instead; see e.g. the Southern Ocean Continuous Plankton Recorder source for an example")
+    stop("bb_handler_aadc is hard-deprecated due to changes on the AADC servers. Use bb_handler_rget (see e.g. `sources('GVDEM_2008')` for an example) or bb_handler_aws_s3 (see `sources('SO-CPR')`)")
+}
+
+#' Generate a bowerbird data source object for an Australian Antarctic Data Centre data set
+#'
+#' @param metadata_id string: the metadata ID of the data set. Browse the AADC's collection at https://data.aad.gov.au/metadata/records/ to find the relevant \code{metadata_id}
+#'
+#' @return A tibble containing the data source definition, as would be returned by \code{\link[bowerbird]{bb_source}}
+#'
+#' @seealso \code{\link{bb_source}}
+#'
+#' @examples
+#' \dontrun{
+#'   ## generate the source def for the "AADC-00009" dataset
+#'   ##  (Antarctic Fur Seal Populations on Heard Island, Summer 1987-1988)
+#'   src <- bb_aadc_source("AADC-00009")
+#'
+#'   ## download it to a temporary directory
+#'   data_dir <- tempfile()
+#'   dir.create(data_dir)
+#'   res <- bb_get(src, local_file_root = data_dir, verbose = TRUE)
+#'   res$files
+#' }
+#'
+#' @export
+bb_aadc_source <- function(metadata_id) {
+    assert_that(is.string(metadata_id))
+    if (grepl("^http", metadata_id)) metadata_id <- basename(metadata_id)
+    md <- get_aadc_md(metadata_id)
+    murl <- paste0("https://data.aad.gov.au/metadata/records/", metadata_id)
+    doi <- tryCatch(sub("^doi:", "", md$data$data_set_citation$dataset_doi), error = function(e) NULL)
+    ## get collection size from the S3 API
+    csize <- tryCatch({
+        s3x <- get_aadc_json(paste0("https://data.aad.gov.au/s3/api/bucket/datasets/science/", metadata_id, "/?export=json"))
+        sz <- sum(s3x$size, na.rm = TRUE)/1024^3 ## in GB
+        if (!is.na(sz)) ceiling(sz*10)/10 else NULL
+    }, error = function(e) NULL)
+    bb_source(name = md$data$entry_title,
+              id = if (length(doi) > 0) doi else metadata_id,
+              description = md$data$summary$abstract,
+              doc_url = if (length(doi) > 0) paste0("https://doi.org/", doi) else murl,
+              citation = md$data$citation,
+              license = "CC-BY",
+              method = list("bb_handler_aws_s3", bucket = "datasets", base_url = "services.aad.gov.au", region = "public", prefix = paste0("science/", metadata_id), use_https = FALSE),
+              comment = "Unusual spec of region and base_url is a workaround for an aws.s3 issue, see https://github.com/cloudyr/aws.s3/issues/318",
+              postprocess = NULL,
+              collection_size = csize)
+}
+
+## not exported, for internal use
+bb_aadc_s3_source_gen <- function(metadata_id, name = NULL, id = NULL, doi = NULL, description, citation, method_args = list(), collection_size = NULL, data_group = NULL, access_function = NULL) {
+    assert_that(is.string(metadata_id))
+    if (grepl("^http", metadata_id)) metadata_id <- basename(metadata_id)
+    murl <- paste0("https://data.aad.gov.au/metadata/records/", metadata_id)
+    pretty_name <- gsub("[[:punct:]_]+", " ", metadata_id)
+    bb_source(name = if (!is.null(name)) name else pretty_name,
+              id = if (!is.null(id)) id else if (length(doi) > 0) doi else metadata_id,
+              description = description,
+              doc_url = paste0("https://doi.org/", doi),
+              citation = if (!is.null(citation)) citation else "See documentation URL",
+              license = "CC-BY",
+              method = c(list("bb_handler_aws_s3", bucket = "datasets", base_url = "services.aad.gov.au", region = "public", prefix = paste0("science/", metadata_id), use_https = FALSE), method_args),
+              comment = "Unusual spec of region and base_url is a workaround for an aws.s3 issue, see https://github.com/cloudyr/aws.s3/issues/318",
+              postprocess = NULL,
+              collection_size = collection_size,
+              data_group = data_group,
+              if (!is.null(access_function)) access_function = access_function)
 }
 
