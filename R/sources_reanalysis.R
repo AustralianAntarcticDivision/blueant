c9875427e00289aa54ce05fc87db3d58e655b224
Ben Raymond <ben.raymond@aad.gov.au>
2022-05-05 11:27:28 +1000

    add CCMP wind source

--- R/sources_reanalysis.R
+++ R/sources_reanalysis.R
@@ -5,6 +5,8 @@
 #' \itemize{
 #'   \item "NCEP-DOE Reanalysis 2": NCEP-DOE Reanalysis 2 is an improved version of the NCEP Reanalysis I model that fixed errors and updated paramterizations of of physical processes. Accepts \code{time_resolution} values of "6 hour", "day", and/or "month" (default). The 6-hourly data is the original output time resolution. Daily and monthly averages are calculated from the 6-hourly model output
 #'   \item "NCEP/NCAR Reanalysis 1": The NCEP/NCAR Reanalysis 1 project is using a state-of-the-art analysis/forecast system to perform data assimilation using past data from 1948 to the present. Only the monthly data are so far included here
+#'   \item "CCMP Wind Product V2": The Cross-Calibrated Multi-Platform (CCMP) gridded surface vector winds are produced using satellite, moored buoy, and model wind data, and are a Level-3 ocean vector wind analysis product. The V2 CCMP processing combines Version-7 RSS radiometer wind speeds, QuikSCAT and ASCAT scatterometer wind vectors, moored buoy wind data, and ERA-Interim model wind fields using a Variational Analysis Method (VAM) to produce four maps daily of 0.25 degree gridded vector winds
+#   \item "NOAA-CIRES-DOE Twentieth Century Reanalysis V3": NOAA-CIRES-DOE 20th Century Reanalysis V3 contains objectively-analyzed 4-dimensional weather maps and their uncertainty from the early 19th century to the 21st century
 #' }
 #'
 #' The returned tibble contains more information about each source.
@@ -112,6 +114,21 @@ sources_reanalysis <- function(name,formats,time_resolutions, ...) {
                              postprocess = NULL,
                              collection_size = 2,
                              data_group = "Reanalysis"))
+    }
+    if (is.null(name) || any(name %in% tolower(c("CCMP Wind Product V2", "CCMPv2")))) {
+        out <- rbind(out,
+                     bb_source(
+                         name = "CCMP Wind Product V2",
+                         id = "CCMPv2",
+                         description = "The Cross-Calibrated Multi-Platform (CCMP) gridded surface vector winds are produced using satellite, moored buoy, and model wind data, and are a Level-3 ocean vector wind analysis product. The V2 CCMP processing combines Version-7 RSS radiometer wind speeds, QuikSCAT and ASCAT scatterometer wind vectors, moored buoy wind data, and ERA-Interim model wind fields using a Variational Analysis Method (VAM) to produce four maps daily of 0.25 degree gridded vector winds",
+                         doc_url = "https://www.remss.com/measurements/ccmp/",
+                         citation = "Wentz FJ, Scott J, Hoffman R, Leidner M, Atlas R, Ardizzone J (2015) Remote Sensing Systems Cross-Calibrated Multi-Platform (CCMP) 6-hourly ocean vector wind analysis product on 0.25 deg grid, Version 2.0, [indicate date subset, if used]. Remote Sensing Systems, Santa Rosa, CA. Available online at www.remss.com/measurements/ccmp\n\nMears CA, Scott J, Wentz FJ, Ricciardulli L, Leidner SM, Hoffman R, Atlas R (2019) A near real time version of the Cross Calibrated Multiplatform (CCMP) ocean surface wind velocity data set. Journal of Geophysical Research: Oceans. https://doi.org/10.1029/2019JC015367",
+                         source_url = "https://data.remss.com/ccmp/",
+                         license = "Please cite",
+                         method = list("bb_handler_rget", level = 4, no_parent = FALSE, accept_follow = "data\\.remss\\.com/ccmp"),
+                         postprocess = NULL,
+                         collection_size = 120,
+                         data_group = "Reanalysis"))
     }
     out
 }
