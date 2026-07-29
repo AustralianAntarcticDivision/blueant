3e7a54c9e69880e00e0ba671d8766737efb11c96
Ben Raymond <ben.raymond@aad.gov.au>
2026-07-29 15:55:59 +1000

    modify aadc test for unannounced aadc change

--- tests/testthat/test_aadc.R
+++ tests/testthat/test_aadc.R
@@ -3,7 +3,7 @@ test_that("The AADC S3 source generator works", {
     src <- sources_biological("SO-CPR")
     temp_root <- tempdir()
     cf <- bb_add(bb_config(local_file_root = temp_root), src)
-    expect_true(grepl("data.aad.gov.au/eds/api/dataset/[^/]+/AADC-00099/?$", bb_data_source_dir(cf)))
+    expect_true(all(grepl("data.aad.gov.au/eds/api/dataset/[^/]+/AADC-00099/?$", bb_data_source_dir(cf))))
     status <- bb_sync(cf, confirm_downloads_larger_than = NULL)
     expect_gt(nrow(status$files[[1]]), 1L) ## at least two files
     expect_true(all(file.exists(status$files[[1]]$file)))
