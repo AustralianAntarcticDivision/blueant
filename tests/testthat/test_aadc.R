a4820bbbbe7de6d60812ffbe79abaa861a8a9c89
Ben Raymond <ben.raymond@aad.gov.au>
2026-07-31 11:01:58 +1000

    revert 3e7a following aadc fix

--- tests/testthat/test_aadc.R
+++ tests/testthat/test_aadc.R
@@ -3,7 +3,8 @@ test_that("The AADC S3 source generator works", {
     src <- sources_biological("SO-CPR")
     temp_root <- tempdir()
     cf <- bb_add(bb_config(local_file_root = temp_root), src)
-    expect_true(all(grepl("data.aad.gov.au/eds/api/dataset/[^/]+/AADC-00099/?$", bb_data_source_dir(cf))))
+    expect_equal(length(bb_data_source_dir(cf)), 1) ## should only be one data set. The metadata record should link to the actual (current) data plus the logbooks, but the latter are filtered out by the source constructor
+    expect_true(grepl("data.aad.gov.au/eds/api/dataset/[^/]+/AADC-00099/?$", bb_data_source_dir(cf)))
     status <- bb_sync(cf, confirm_downloads_larger_than = NULL)
     expect_gt(nrow(status$files[[1]]), 1L) ## at least two files
     expect_true(all(file.exists(status$files[[1]]$file)))
